6c47334bb615af3f071591310c181ec701ba709b
Johann Chen <johann@wattzon.com>
2020-05-13 16:37:33 -0700

    correct scope name

--- packages/cra-app/src/App.tsx
+++ packages/cra-app/src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { MyNewComponent } from "@my-org/ui-story";
+import { MyNewComponent } from "@johannchen/ui-story";
 
 function App() {
   return (
